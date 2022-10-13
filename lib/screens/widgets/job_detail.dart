ba2965d3965fb5622cd1d25cf570fa87d710838b
juanpc10 <juancarrillodev@gmail.com>
2022-10-12 20:37:26 -0500

    Alignament in job_details.dart fixed

--- lib/screens/widgets/job_detail.dart
+++ lib/screens/widgets/job_detail.dart
@@ -22,8 +22,8 @@ class JobDetail extends StatelessWidget {
       height: 550,
       child: SingleChildScrollView(
         child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
               color: Colors.grey.withOpacity(0.3),
@@ -34,114 +34,118 @@ class JobDetail extends StatelessWidget {
             SizedBox(
               height: 30,
             ),
-            Row(
+            Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Container(
-                      height: 40,
-                      padding: EdgeInsets.all(9),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.grey.withOpacity(0.1)),
-                      child: Image.asset(job.logoUrl),
-                    ),
-                    SizedBox(width: 20),
-                    Text(
-                      job.company,
-                      style: TextStyle(fontSize: 16),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Icon(
-                        job.isMark
-                            ? Icons.bookmark
-                            : Icons.bookmark_outline_outlined,
-                        color: job.isMark
-                            ? Theme.of(context).primaryColor
-                            : Colors.black),
-                    Icon(Icons.more_horiz_outlined),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Text(
-              job.title,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconText(Icons.location_on_outlined, job.location),
-                IconText(Icons.access_time_outlined, job.time),
-              ],
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              'Requirements',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            ...job.req
-                .map((e) => Container(
-                    margin: EdgeInsets.symmetric(vertical: 5),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    Row(
                       children: [
                         Container(
-                          margin: EdgeInsets.only(
-                            top: 10,
-                          ),
-                          height: 5,
-                          width: 5,
-                          // Lista
+                          height: 40,
+                          padding: EdgeInsets.all(9),
                           decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.black,
-                          ),
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.grey.withOpacity(0.1)),
+                          child: Image.asset(job.logoUrl),
                         ),
-                        SizedBox(
-                          width: 10,
+                        SizedBox(width: 20),
+                        Text(
+                          job.company,
+                          style: TextStyle(fontSize: 16),
                         ),
-                        ConstrainedBox(
-                          constraints: BoxConstraints(
-                            maxWidth: 300,
-                          ),
-                          child: Text(
-                            e,
-                            style: TextStyle(wordSpacing: 2.5, height: 1.5),
-                          ),
-                        )
                       ],
-                    )))
-                .toList(),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 25),
-              height: 45,
-              width: double.maxFinite,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    elevation: 0,
-                    backgroundColor: Theme.of(context).primaryColor),
-                child: Text('Apply now'),
-              ),
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                            job.isMark
+                                ? Icons.bookmark
+                                : Icons.bookmark_outline_outlined,
+                            color: job.isMark
+                                ? Theme.of(context).primaryColor
+                                : Colors.black),
+                        Icon(Icons.more_horiz_outlined),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text(
+                  job.title,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconText(Icons.location_on_outlined, job.location),
+                    IconText(Icons.access_time_outlined, job.time),
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  'Requirements',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                ...job.req
+                    .map((e) => Container(
+                        margin: EdgeInsets.symmetric(vertical: 5),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(
+                                top: 10,
+                              ),
+                              height: 5,
+                              width: 5,
+                              // Lista
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.black,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            ConstrainedBox(
+                              constraints: BoxConstraints(
+                                maxWidth: 300,
+                              ),
+                              child: Text(
+                                e,
+                                style: TextStyle(wordSpacing: 2.5, height: 1.5),
+                              ),
+                            )
+                          ],
+                        )))
+                    .toList(),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 25),
+                  height: 45,
+                  width: double.maxFinite,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        elevation: 0,
+                        backgroundColor: Theme.of(context).primaryColor),
+                    child: Text('Apply now'),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
