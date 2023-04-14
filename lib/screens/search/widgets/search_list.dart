7d41c68902c270f6d0457cbb6bdbe4665bfb7ad9
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2023-04-13 19:27:36 -0600

    advancements to display the details of vacancies in Firebase

--- lib/screens/search/widgets/search_list.dart
+++ lib/screens/search/widgets/search_list.dart
@@ -12,7 +12,7 @@ class SearchList extends StatefulWidget {
 }
 
 class _searchList extends State<SearchList> {
-  final databaseRef = FirebaseDatabase.instance.ref('example').child('jobs');
+  final databaseRef = FirebaseDatabase.instance.ref('jobs');
   @override
   Widget build(BuildContext context) {
     final widthSize = MediaQuery.of(context).size.width;
@@ -31,76 +31,95 @@ class _searchList extends State<SearchList> {
           // return Text('hello');
           var x = value['name'];
           print('x -> $x');
-          return Container(
-            margin: EdgeInsets.only(top: 25),
-            width: 270,
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: isMobile(context) ? Colors.white : kSecundaryDark,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          return InkWell(
+              onTap: () {
+                // Acciones a realizar al presionar el Container
+                showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context) {
+                    // Crea el contenido del modal dentro del contexto del Container
+                    return Container(
+                      height: 200,
+                      width: double.infinity,
+                      color: Colors.white,
+                      child: Center(
+                        child: Text('Este es el contenido del modal'),
+                      ),
+                    );
+                  },
+                );
+              },
+              child: Container(
+                margin: EdgeInsets.only(top: 25),
+                width: 270,
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: isMobile(context) ? Colors.white : kSecundaryDark,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Container(
-                          height: 40,
-                          width: 40,
-                          padding: EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.grey.withOpacity(0.1),
-                          ),
-                          child: Image.asset(value['logoUrl']),
+                        Row(
+                          children: [
+                            Container(
+                              height: 40,
+                              width: 40,
+                              padding: EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.grey.withOpacity(0.1),
+                              ),
+                              child: Image.asset(value['logoUrl']),
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              value['company'],
+                              style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
                         ),
-                        SizedBox(width: 10),
-                        Text(
-                          value['company'],
-                          style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold),
-                        ),
+                        // Icon saved
+                        Icon(
+                            value['isMark']
+                                ? Icons.bookmark
+                                : Icons.bookmark_outline_outlined,
+                            color: value['isMark']
+                                ? Theme.of(context).primaryColor
+                                : isDesktop(context)
+                                    ? Colors.white
+                                    : Colors.black)
                       ],
                     ),
-                    // Icon saved
-                    Icon(
-                        value['isMark']
-                            ? Icons.bookmark
-                            : Icons.bookmark_outline_outlined,
-                        color: value['isMark']
-                            ? Theme.of(context).primaryColor
-                            : isDesktop(context)
-                                ? Colors.white
-                                : Colors.black)
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      value['title'],
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color:
+                              isDesktop(context) ? Colors.grey.shade300 : null),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconText(Icons.location_on_outlined, value['location']),
+                        IconText(Icons.access_time_outlined, value['time'])
+                      ],
+                    ),
                   ],
                 ),
-                SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  value['title'],
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: isDesktop(context) ? Colors.grey.shade300 : null),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconText(Icons.location_on_outlined, value['location']),
-                    IconText(Icons.access_time_outlined, value['time'])
-                  ],
-                ),
-              ],
-            ),
-          );
+              ));
         },
       )),
     );
---
+++ lib/screens/widgets/job_listFirebase.dart
@@ -0,0 +1,36 @@
+import 'package:examen/models/job.dart';
+import 'package:examen/screens/widgets/job_detail.dart';
+import 'package:examen/screens/widgets/job_item.dart';
+import 'package:flutter/material.dart';
+
+class JobList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final jobList = Job.generateJobs(context);
+    print('HOLA $jobList');
+    return Container(
+        margin: EdgeInsets.symmetric(
+          vertical: 25,
+        ),
+        height: 160,
+        child: ListView.separated(
+            padding: EdgeInsets.symmetric(
+              horizontal: 25,
+            ),
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) => GestureDetector(
+                  onTap: () {
+                    showModalBottomSheet(
+                        backgroundColor: Colors.transparent,
+                        isScrollControlled: true,
+                        context: context,
+                        builder: ((context) => JobDetail(jobList[index])));
+                  },
+                  child: JobItem(jobList[index]),
+                ),
+            separatorBuilder: (_, index) => SizedBox(
+                  width: 15,
+                ),
+            itemCount: jobList.length));
+  }
+}
